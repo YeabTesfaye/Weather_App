2fd1f25402c657e896d1757d519f2800225c63ea
Yeabsra <tesfayeyeabsra446@gmail.com>
2023-09-26 18:02:04 +0300

    retrive city datas from local storage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -33,6 +33,14 @@
         </template>
       </ul>
     </div>
+    <div class="flex flex-col gap-4">
+      <Suspense>
+        <CityList />
+        <template #fallback>
+          <p>Loading</p>
+        </template>
+      </Suspense>
+    </div>
   </main>
 </template>
 
@@ -40,7 +48,7 @@
 import { ref } from "vue";
 import axios from "axios";
 import { useRouter } from "vue-router";
-
+import CityList from "../components/CityList.vue";
 const router = useRouter();
 const previewCity = (searchResult) => {
   const [city, state] = searchResult.place_name.split(",");
