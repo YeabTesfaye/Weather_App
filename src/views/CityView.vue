b4fdbffa577430bb6a5e0d0c485034782b102f51
Yeabsra <tesfayeyeabsra446@gmail.com>
2023-09-26 16:52:40 +0300

    add city view

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,9 +1,14 @@
 <template>
-    <div>
-
-    </div>
+  <div>
+    <Suspense>
+      <AsyncCityVue />
+      <template #fallback>
+        <p >Loading</p>
+      </template>
+    </Suspense>
+  </div>
 </template>
 
 <script setup>
-
+import AsyncCityVue from "../components/AsyncCityVue.vue";
 </script>
