5f9b527ce5b24f4a7d52b6ca577167811db8ebde
Yeabsra <tesfayeyeabsra446@gmail.com>
2023-09-26 19:25:59 +0300

    add router transition

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,32 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-    history : createWebHistory(),
-    routes : [
-        {
-            path : "/",
-            name : "home",
-            component : () => import('../views/HomeView.vue')
-        },
-        {
-            path : "/weather/:state/:city",
-            name : "cityView",
-            component: () => import('../views/CityView.vue')
-        }
-    ]
-})
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../views/HomeView.vue"),
+      meta: {
+        title: "Home",
+      },
+    },
+    {
+      path: "/weather/:state/:city",
+      name: "cityView",
+      component: () => import("../views/CityView.vue"),
+      meta: {
+        title: "Weather",
+      },
+    },
+  ],
+});
+
+router.beforeResolve((to, from, next) => {
+  document.title = `${
+    to.params.state ? `${to.params.city}, ${to.params.state}` : to.meta.title
+  } | The Local Weather`;
+  next();
+});
 
 export default router;
